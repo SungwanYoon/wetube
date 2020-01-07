ee0e2d5a4ec5e2275797f7a3b1fe6b60b4e5d48a
SungwanYoon <57616919+SungwanYoon@users.noreply.github.com>
2020-01-08 00:30:17 +0900

    20.01.08_v1

--- app.js
+++ app.js
@@ -3,19 +3,25 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 
 const app = express();
+app.set("view engine", "pug");
 
+// Cookie data parser
 app.use(cookieParser());
+// Body form data parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// Security
 app.use(helmet());
+// Logging
 app.use(morgan("combined"));
 
+// Express Router
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
