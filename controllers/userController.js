aec68c2a979d722881f79588b44702da035226f5
SungwanYoon <SungwanYoon@users.noreply.github.com>
2020-01-10 19:14:18 +0900

    20.01.10

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,29 @@
-export const join = (req, res) => res.render("join", { pageTitle: "join" });
-export const login = (req, res) => res.render("login", { pageTitle: "login" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "join" });
+};
+
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "join" });
+  } else {
+    // To Do: Register User
+    // To Do: Log User In
+    res.redirect(routes.home);
+  }
+};
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Log In" });
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "logout" });
 export const userDetail = (req, res) =>
