e011156f56b27cff085a4aff502f3daedb38305a
SungwanYoon <SungwanYoon@users.noreply.github.com>
2020-01-13 21:44:32 +0900

    20.01.13

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,8 +24,9 @@ export const postLogin = (req, res) => {
   res.redirect(routes.home);
 };
 
-export const logout = (req, res) =>
-  res.render("logout", { pageTitle: "logout" });
+export const logout = (req, res) => {
+  res.redirect(routes.home);
+};
 export const userDetail = (req, res) =>
   res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) =>
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,14 @@
-import { videos } from "../db";
+import routes from "../routes";
+import Video from "../models/Video";
 
-export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
 };
 
 export const search = (req, res) => {
@@ -10,8 +17,16 @@ export const search = (req, res) => {
   } = req;
   res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
-export const upload = (req, res) =>
+export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, description }
+  } = req;
+
+  res.redirect(routes.videoDetail(123456));
+};
 
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
