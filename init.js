5075916bab6fcce78e55ad4ceb5ed2588cb2da9a
SungwanYoon <SungwanYoon@users.noreply.github.com>
2020-01-17 17:07:08 +0900

    20.01.17

--- init.js
+++ init.js
@@ -1,14 +1,16 @@
-import "./db";
-import app from "./app";
-import dotenv from "dotenv";
+import '@babel/polyfill';
+import dotenv from 'dotenv';
+import './db';
+import app from './app';
+
 dotenv.config();
-import "./models/Video";
-import "./models/Comment";
 
-const PORT = process.env.PORT;
+import './models/Video';
+import './models/Comment';
+import './models/User';
 
-const handleListening = () => {
-  console.log(`✔ Listening on : http://localhost:${PORT}`);
-};
+const PORT = process.env.PORT || 4000;
+
+const handleListening = () => console.log(`✅  Listening on: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
--- routes.js
+++ routes.js
@@ -6,17 +6,36 @@ const LOGOUT = "/logout";
 const SEARCH = "/search";
 
 // Users
+
 const USERS = "/users";
 const USER_DETAIL = "/:id";
 const EDIT_PROFILE = "/edit-profile";
 const CHANGE_PASSWORD = "/change-password";
+const ME = "/me";
 
 // Videos
+
 const VIDEOS = "/videos";
 const UPLOAD = "/upload";
 const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
+
+// Github
+
+const GITHUB = "/auth/github";
+const GITHUB_CALLBACK = "/auth/github/callback";
+
+// Facebook
+
+const FB = "/auth/facebook";
+const FB_CALLBACK = "/auth/facebook/callback";
+
+// API
+
+const API = "/api";
+const REGISTER_VIEW = "/:id/view";
+const ADD_COMMENT = "/:id/comment";
 
 const routes = {
   home: HOME,
@@ -52,11 +71,19 @@ const routes = {
   },
   deleteVideo: id => {
     if (id) {
-      return `/video/${id}/delete`;
+      return `/videos/${id}/delete`;
     } else {
       return DELETE_VIDEO;
     }
-  }
+  },
+  gitHub: GITHUB,
+  githubCallback: GITHUB_CALLBACK,
+  me: ME,
+  facebook: FB,
+  facebookCallback: FB_CALLBACK,
+  api: API,
+  registerView: REGISTER_VIEW,
+  addComment: ADD_COMMENT
 };
 
 export default routes;
