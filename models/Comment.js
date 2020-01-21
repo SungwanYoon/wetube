43b86ff786c3f5531f43f668940d1f449a6cbc18
SungwanYoon <SungwanYoon@users.noreply.github.com>
2020-01-21 18:51:11 +0900

    20.01.21

--- models/Comment.js
+++ models/Comment.js
@@ -1,9 +1,9 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const CommentSchema = new mongoose.Schema({
   text: {
     type: String,
-    required: "Text is required"
+    required: 'Text is required'
   },
   createdAt: {
     type: Date,
@@ -11,9 +11,9 @@ const CommentSchema = new mongoose.Schema({
   },
   creator: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User"
+    ref: 'User'
   }
 });
 
-const model = mongoose.model("Comment", CommentSchema);
+const model = mongoose.model('Comment', CommentSchema);
 export default model;
